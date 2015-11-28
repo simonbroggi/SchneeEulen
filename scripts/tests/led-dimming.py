29b24dde98e60fffc21179a61d5c787d98c47fe4
Kai Jauslin <kj@nextension.com>
2015-11-28 17:22:06 +0000

    Added threaded led dimming test

--- scripts/tests/led-dimming.py
+++ scripts/tests/led-dimming.py
@@ -1,40 +1,53 @@
 #!/usr/bin/env python
 
 import time, sys, random, math
+import threading
 import pigpio
 
 pi = pigpio.pi() # connect to local Pi
 
-start_time = time.time()
-
-##while (time.time()-start_time) < 20:
-##   R = random.randrange(0, 255, 1)
-##   pi.set_PWM_dutycycle(27, R)
-##   time.sleep(0.02)
-
-steps = 40000
+steps = 1000
 pi.set_PWM_range(27, steps)
-print pi.get_PWM_range(27)
-pi.set_PWM_frequency(27, 10000000)
+pi.set_PWM_frequency(27, 100)
 
 def dutycycle(val):
-   return (1.0 - math.log(steps-val) / math.log(steps))
+   ## val = max(min(val, steps), 0)
+   ## return steps*(1.0 - math.log(steps-val, steps))
+   return val 
 
-def godown():
-   for t in range(steps-1, 1, -1):
-      #print dutycycle(t)*steps
-      pi.set_PWM_dutycycle(27,dutycycle(t)*steps)
-      #time.sleep(0.0001)
 
-def goup():
-   for t in range(1, steps-1, 1):
-      pi.set_PWM_dutycycle(27,dutycycle(t)*steps)
-      time.sleep(0.0001)
+def dimmer(pin, start_val, end_val, duration, step_size = 1):
+   pi = pigpio.pi()
+   
+   print "dimmer worker on pin %s start=%s end=%s, duration=%s" % (pin, start_val, end_val, duration)
+   start_time = time.time()
 
-godown()
-goup()
+   if start_val > end_val and step_size > 0:
+      step_size = - step_size
 
-pi.set_PWM_dutycycle(27, 255)
-time.sleep(5)
+   start_step = int(round(start_val*(steps - 1.0)))
+   end_step = int(round(end_val*(steps - 1.0)))
+   step_count = abs(end_step - start_step + 1)
+
+   step_delay = duration / step_count
+   print "start=",start_step,"end=",end_step,"delay=",step_delay
+   
+   for t in range(start_step, end_step, step_size):
+      ## print t, dutycycle(t)
+      pi.set_PWM_dutycycle(pin, dutycycle(t))
+      time.sleep(step_delay)
+
+   print time.time() - start_time
+   pi.stop()
+   return
+
+
+t = threading.Thread(target=dimmer, args=(27, 0.0, 1.0, 0.5, 1))
+t.start()
+t.join()
+t = threading.Thread(target=dimmer, args=(27, 1.0, 0.0, 0.5, -1))
+t.start()
+t.join()
+
 pi.set_PWM_dutycycle(27, 0)
 pi.stop()
