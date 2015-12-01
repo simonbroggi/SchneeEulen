077d49a01282328f0ae9e31658bb61911ffc0756
Kai Jauslin <kai.jauslin@cando-image.com>
2015-12-01 23:14:21 +0100

    Added lights pin configuration

--- scripts/conf.template.py
+++ scripts/conf.template.py
@@ -16,16 +16,21 @@ LOG_LEVEL = logging.DEBUG
 
 # id for this specific client - must be unique in the network
 CLIENT_ID = 'SNOWCHILD'
+LIGHT_PINS = {
+    'eye_left': 27,
+    'eye_right': 24,
+    'body': 28
+}
 
 # networking
 NETWORK_CONNECT_RETRY_DELAY = 3
 
 # information about where to send data
-CLIENT_MASTER_IP = '10.0.0.1'
+CLIENT_MASTER_IP = '127.0.0.1'
 CLIENT_MASTER_PORT = 12345
 
 #
 # master configuration
 #
-MASTER_IP = '10.0.0.1'
+MASTER_IP = '127.0.0.1'
 MASTER_PORT = 12345
