2f216e48ffa879f25eb07b4823653fe27825327f
Kai Jauslin <kai.jauslin@cando-image.com>
2015-12-01 23:13:40 +0100

    First runnable version of master

--- scripts/snowly-master.py
+++ scripts/snowly-master.py
@@ -9,45 +9,45 @@ from time import sleep
 from PodSixNet.Channel import Channel
 from PodSixNet.Server import Server
 
+# logging configuration
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] (%(threadName)-10s) %(message)s')
+
 
 """
-LENZERHEIDE ZAUBERWALD 2015 * SCHNEE-EULEN (master)
+LENZERHEIDE ZAUBERWALD 2015 * SCHNEE-EULEN * Snowy owls (master)
 """
-class RabbitHole(Channel):
+class SnowlyChannel(Channel):
     """
-    A rabbit hole is the channel for one connected client pi.
+    Networking channel for one connected client pi.
     """
     def __init__(self, *args, **kwargs):
         Channel.__init__(self, *args, **kwargs)
 
     def Network_input(self, data):
-        print "rabbit client: input changed: ", data
+        log.debug("snowl client: input changed: %s", data)
 
     def Network_config(self, data):
-        print "received client config", data
+        log.debug("received client config: %s", data)
         # insert and map client inputs/outputs
         self._server.update_clients(self, data)
 
-    def Network_carrot(self, data):
-        print "rabbit got carrot", data
+    def Network_mouse(self, data):
+        log.debug("custom action (test): mouse")
+        log.debug("---> snowy owl got mouse" % data)
 
     def Network_input(self, data):
-        print "received input from client ", data
+        log.debug("received input from client " % data)
         self._server.update_input(self, data)
 
     def Close(self):
         self._server.remove_client(self)
 
 
-class WhiteRabbitServer(Server):
-    channelClass = RabbitHole
+class SnowlyServer(Server):
+    channelClass = SnowlyChannel
     exitSignal = False
 
-    inputs = {}
-    outputs = {}
-    actions = {}
-    virtual_inputs = []
-    virtual_outputs = []
     framerate = 20
     last_time = 0
     current_time = 0
@@ -57,146 +57,51 @@ class WhiteRabbitServer(Server):
         self.id = conf.CLIENT_ID
         self.conf = kwargs['conf']
         del kwargs['conf']
-        print 'master.conf',self.conf
+        log.debug('using master.conf from %s', self.conf)
 
         Server.__init__(self, *args, **kwargs)
         self.clients = WeakKeyDictionary()
-        print 'Server launched'
+        log.debug('Server launched')
 
     def Connected(self, channel, addr):
-        print 'new rabbit hole: ', channel
+        log.debug('new channel: %s', channel)
 
     def update_clients(self, channel, data):
-        print 'updating client information on server:', channel, data
+        log.debug('updating client information on server: channel=%s data=%s' % (channel, data))
 
         # store client data
         self.clients[channel] = {
-            'numInputs': data['inputs'],
-            'numOutputs': data['outputs'],
-            'outputWeight': data['outputWeight'],
-            'inputWeight': data['inputWeight']
+            'id': data['id'],
         }
-
-        # update virtual input and output mappings
-        self.map_clients()
-
-    def map_clients(self):
-        # generate virtual map of all available outputs
-        print self.clients.items()
-        for item in self.clients.items():
-            print item
-
-        ordered_clients = sorted(self.clients.items(), key=lambda x: x[1]['outputWeight'])
-        self.virtual_outputs = []
-        for client in ordered_clients:
-            for i in range(client[1]['numOutputs']):
-                self.virtual_outputs = self.virtual_outputs + [{
-                    'client': client,
-                    'localIndex': i,
-                    'val': None
-                }]
-
-        for output in self.virtual_outputs:
-            print output
-
-        # generate virtual map of all available inputs
-        ordered_clients = sorted(self.clients.items(), key=lambda x: x[1]['inputWeight'])
-        self.virtual_inputs = []
-        for client in ordered_clients:
-            for i in range(client[1]['numInputs']):
-                self.virtual_inputs = self.virtual_inputs + [{
-                    'client': client,
-                    'localIndex': i,
-                    'val': None
-                }]
-
-    # set a new value of an output pin with a virtual index
-    # will send value to client
-    def set_virtual_out(self, index, val):
-        if index >= len(self.virtual_outputs):
-            # na
-            print "output "+str(index)+" not available"
-            return
-
-        output_info = self.virtual_outputs[index]
-        output_info['val'] = val
-        client = output_info['client'][0]
-
-        # ask client to set the value on the mapped index
-        client.Send({
-            'action': 'setoutput',
-            'index': output_info['localIndex'],
-            'val': val
-        })
-
-        # we don't care about feedback... :-)
-
-    # get the last known value of an input pin with a virtual index
-    def get_virtual_in(self, index):
-        if index > len(self.virtual_inputs):
-            # na
-            print "input "+index+" not available"
-
-        client_info = self.virtual_inputs[index]
-        return client_info['val']
-
-    def update_input(self, client, data):
-        print 'server update input', client, data
-        local_index = int(data['channel'])
-        val = data['val']
-        print 'local_index', local_index, 'val', val
-
-        # map local input to virtualized inputs
-        for i,_ in enumerate(self.virtual_inputs):
-            print(_['client'][0], _['localIndex'])
-
-        matches = [i for i,_ in enumerate(self.virtual_inputs)
-                 if (_['client'][0] == client and _['localIndex'] == local_index)]
-
-        if len(matches) > 0:
-            print 'setting input ', self.virtual_inputs[matches[0]], ' to val=', val
-            self.virtual_inputs[matches[0]]['val'] = val
 
     def remove_client(self, channel):
         print 'removing client', channel
         del self.clients[channel]
-        self.map_clients()
 
+
+    """
+    Main loop
+    """
     def launch(self, stdscr=None, *args, **kwds):
-        print "White Rabbit Master ready"
+        print "Snowly Server ready"
         while not self.exitSignal:
             self.current_time = time.time()
             self.delta_time = self.current_time - self.last_time
             if self.delta_time >= (1.0/self.framerate):
-                self.run_actions()
+                #self.run_actions()
                 self.last_time = self.current_time
 
-            self.Pump()
+            try:
+                self.Pump()
+            except Exception as e:
+                # catch and log any exceptions that come up to this point
+                log.error(e)
 
             # sleep smallest interval possible on system (~1-10ms)
             time.sleep(0.0001)
 
-    def run_actions(self):
-        ordered_actions = sorted(self.actions.items(), key=lambda x: x[1]['weight'])
 
-        for action in ordered_actions:
-            action[0].update(self.current_time, self.delta_time)
 
-    def register_action(self, action, weight):
-        if not isinstance(action, Action):
-            raise BaseException("action doesn't have base type Action", action)
-
-        self.actions[action] = {
-            'weight': weight
-        }
-        action.registered({'master': self, 'framerate': self.framerate})
-
-    def remove_action(self, action):
-        del self.actions[action]
-
-# logging configuration
-log = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] (%(threadName)-10s) %(message)s')
 
 # read configuration
 config_file = 'conf'
@@ -211,8 +116,7 @@ conf = __import__(config_file, globals(), locals(), [])
 # update log configuration
 log.setLevel(conf.LOG_LEVEL)
 
-log.debug("Sno")
-print "White Rabbit Master initializing"
+log.debug("Snowly Server initializing at %s:%s" % (conf.MASTER_IP, conf.MASTER_PORT))
 server = SnowlyServer(localaddr=(conf.MASTER_IP, conf.MASTER_PORT), conf=conf)
 
 # add actions
@@ -221,8 +125,6 @@ server = SnowlyServer(localaddr=(conf.MASTER_IP, conf.MASTER_PORT), conf=conf)
 #server.register_action(MultipathBase(config='multipath-left.csv', use_inputs=[0]), 0)
 #server.register_action(MultipathBase(config='multipath-right.csv', use_inputs=[1]), 1)
 #server.register_action(IdleAnimation('multipath-idle.csv', use_inputs=[0,1]), 2)
-
-# TODO: consolidate outputs (how?)
 
 server.launch()
 
