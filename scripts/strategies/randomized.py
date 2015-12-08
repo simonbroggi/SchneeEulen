4d68bf6c502eeaabf50451b62cb4d693d6a71581
Kai Jauslin <kai.jauslin@cando-image.com>
2015-12-08 13:35:37 +0100

    Improved randomized tests

--- scripts/strategies/randomized.py
+++ scripts/strategies/randomized.py
@@ -13,9 +13,9 @@ class Breathing(BaseStrategy):
         while not self.__signalExit__:
             body_dimmer = self.main_thread.get_dimmer('eye_left')
             #logging.debug('**breath start**')
-            body_dimmer.add(0.1, 1.0, 1.0, 2.0, True)
-            body_dimmer.add(1.0, 0.1, 1.0, 2.0)
-            self.wait(5)
+            body_dimmer.add(0.0, 1.0, 1.0, 2)
+            body_dimmer.add(1.0, 0.0, 1.0, 2)
+            self.wait(3)
 
         logging.debug('breathing finished')
 
@@ -43,18 +43,21 @@ class SimpleRandomizedStrategy(BaseStrategy):
         breathing.start()
 
         while not self.__signalExit__:
-            # logging.debug('*twinkle*')
-            # self.twinkle(0)
-            # logging.debug('*wait 10s*')
-            # self.wait(10.0)
             servo = self.main_thread.get_servo('head')
-            servo.add(0, 180.0, 4.0, 2.0)
-            self.wait(10)
+            servo.add(0, 180.0, 1.0, 4.0)
+            #servo.add(180.0, 0, 3.0, 4.0)
+            #logging.debug("body_dimmer start")
+            #body_dimmer = self.main_thread.get_dimmer('eye_left')
+            #body_dimmer.add(0.0, 1.0, 1.0, 1, False)
+            #body_dimmer.add(1.0, 0.0, 1.0, -1)
+
+            logging.debug("randomized self.wait(5)")
+            self.wait(4.0)
 
         # wait for subthread to finish
         breathing.signal_exit()
         while breathing.is_alive():
-            logging.debug('waiting for breathing to finish gracefully')
+            logging.debug('...waiting for breathing to finish gracefully...')
             sleep(0.01)
 
         logging.debug('SimpleRandomizedStrategy finished')
