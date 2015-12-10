7cc176087399b40f11f19e9718bf2b3d4c284445
Kai Jauslin <kai.jauslin@cando-image.com>
2015-12-10 20:53:39 +0100

    Random client strategy (as in video)

--- scripts/strategies/client/simple.py
+++ scripts/strategies/client/simple.py
@@ -2,6 +2,7 @@
 import logging
 import threading
 import pigpio
+import random
 from time import sleep
 from strategies.base import StrategyThread
 
@@ -11,10 +12,10 @@ class Breathing(StrategyThread):
 
     def run(self):
         while not self.__signalExit__:
-            body_dimmer = self.main_thread.get_dimmer('eye_left')
+            body_dimmer = self.main_thread.get_dimmer('body')
             #logging.debug('**breath start**')
-            body_dimmer.add(0.0, 1.0, 1.0, 2)
-            body_dimmer.add(1.0, 0.0, 1.0, 2)
+            body_dimmer.add(0.2, 1.0, 2.0, 2)
+            body_dimmer.add(1.0, 0.2, 2.0, 2)
             self.wait(3)
 
         logging.debug('breathing finished')
@@ -42,9 +43,24 @@ class SimpleRandomizedStrategy(StrategyThread):
         breathing = Breathing(self.main_thread)
         breathing.start()
 
+        head_angle = 0.0
+        last_angle = 0.0
+
         while not self.__signalExit__:
             servo = self.main_thread.get_servo('head')
-            servo.add(0, 180.0, 1.0, 4.0)
+
+            head_angle = random.uniform(0.0, 180.0)
+            servo.add(last_angle, head_angle, random.uniform(2.0, 10.0), 1.0, True)
+            last_angle = head_angle
+
+            eye_left = self.main_thread.get_dimmer('eye_left')
+            eye_left.add(0.0, 1.0, 2.0, 2, True)
+            eye_left.add(1.0, 0.0, 2.0, 2)
+
+            eye_right = self.main_thread.get_dimmer('eye_right')
+            eye_right.add(0.0, 1.0, 2.0, 2, True)
+            eye_right.add(1.0, 0.0, 2.0, 2)
+
             #servo.add(180.0, 0, 3.0, 4.0)
             #logging.debug("body_dimmer start")
             #body_dimmer = self.main_thread.get_dimmer('eye_left')
@@ -52,7 +68,7 @@ class SimpleRandomizedStrategy(StrategyThread):
             #body_dimmer.add(1.0, 0.0, 1.0, -1)
 
             logging.debug("randomized self.wait(5)")
-            self.wait(4.0)
+            self.wait(random.randint(4,10))
 
         # wait for subthread to finish
         breathing.signal_exit()
