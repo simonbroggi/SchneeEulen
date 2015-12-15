24252dd9cf5a6f3f40b2a35fd6247617586a8e9e
Simon Broggi <simon.broggi@gmail.com>
2015-12-15 17:53:55 +0100

    auto strategy breath blink heartbeat

--- scripts/strategies/client/autonomous.py
+++ scripts/strategies/client/autonomous.py
@@ -11,18 +11,20 @@ class BreathingAgitation(StrategyThread):
 
     agitation: float between 0 and 1
     """
-    def __init__(self, main_thread, agitation = 0.5):
+    def __init__(self, main_thread, agitation=0.5):
         StrategyThread.__init__(self, main_thread, 'BreathingAgitation')
         self.agitation = agitation
         self.body_dimmer = self.main_thread.get_dimmer('body')
+        self.dark = 0.15
+        self.light = 0.6
 
     def run(self):
         while not self.__signalExit__:
-            breathTime = 3.0 * (1.5-self.agitation)
-            inhaleTime = breathTime * 0.4
+            breathTime = 3.5 * (1.5-self.agitation)
+            inhaleTime = breathTime * 0.42
             exhaleTime = breathTime - inhaleTime
-            self.body_dimmer.add(0.2, 1.0, inhaleTime, 2)
-            self.body_dimmer.add(1.0, 0.2, exhaleTime, 2)
+            self.body_dimmer.add(float('nan'), self.light, inhaleTime, 1)
+            self.body_dimmer.add(float('nan'), self.dark, exhaleTime, 1)
             self.wait(breathTime)
         logging.debug('breathing finished')
 
@@ -30,35 +32,45 @@ class HeartbeatAgitation(StrategyThread):
     """All lights pulsate like a heartbeat.
     Faster heartbeat when agitation is higher.
     """
-    def __init__(self, main_thread, agitation = 0.5):
+    def __init__(self, main_thread, agitation=0.5):
         StrategyThread.__init__(self, main_thread, 'HeartbeatAgitation')
         self.agitation = agitation
         self.body_dimmer = self.main_thread.get_dimmer('body')
         self.eye_left = self.main_thread.get_dimmer('eye_left')
         self.eye_right = self.main_thread.get_dimmer('eye_right')
         self.low = 0.35
-        self.mid = 0.5
+        self.mid = 0.65
         self.midLow = 0.45
         self.high = 1.0
+        self.eye_delta = - 0.3
 
-    def add(self, start_val, end_val, duration, step_size=40, clear=False):
+    def add(self, start_val, end_val, duration, step_size=2, clear=False):
         self.body_dimmer.add(start_val, end_val, duration, step_size, clear)
-        self.eye_left.add(start_val, end_val, duration, step_size, clear)
-        self.eye_right.add(start_val, end_val, duration, step_size, clear)
-
+        eye_start = start_val + self.eye_delta
+        if eye_start <= 0.0:
+            eye_start = 0.0
+        if eye_start >= 1.0:
+            eye_start = 1.0
+        eye_end = end_val + self.eye_delta
+        if eye_end <= 0.0:
+            eye_end = 0.0
+        if eye_end >= 1.0:
+            eye_end = 1.0
+        self.eye_left.add(start_val + self.eye_delta, end_val + self.eye_delta, duration, step_size, clear)
+        self.eye_right.add(start_val + self.eye_delta, end_val + self.eye_delta, duration, step_size, clear)
 
     def run(self):
         while not self.__signalExit__:
             beatTime = 1.0 * (1.5-self.agitation)
             first = beatTime * 0.2
-            second = beatTime * 0.07
+            second = beatTime * 0.1
             third = beatTime * 0.22
             recover = beatTime - first - second - third
-            self.add(self.low, self.mid, first)
-            self.add(self.mid, self.midLow, second)
-            self.add(self.midLow, self.high, third)
-            self.add(self.high, self.low, recover)
-            self.wait(beatTime)
+            self.add(float('nan'), self.mid, first, 2, True)
+            self.add(float('nan'), self.midLow, second)
+            self.add(float('nan'), self.high, third)
+            self.add(float('nan'), self.low, recover)
+            self.wait(beatTime + 0.3)
         logging.debug('heartbeat finished')
 
 class BlinkingAgitation(StrategyThread):
@@ -66,25 +78,25 @@ class BlinkingAgitation(StrategyThread):
 
     agitation: float between 0 and 1
     """
-    def __init__(self, main_thread, agitation = 0.5):
+    def __init__(self, main_thread, agitation=0.5):
         StrategyThread.__init__(self, main_thread, 'BlinkingAgitation')
         self.agitation = agitation
-        self.open = 0.7  # 0.3
-        self.closed = 0.05  # 0.05
+        self.open = 0.11  # 0.3
+        self.closed = 0.005  # 0.05
         self.eye_left = self.main_thread.get_dimmer('eye_left')
         self.eye_right = self.main_thread.get_dimmer('eye_right')
 
-    def add(self, start_val, end_val, duration, step_size=10, clear=False):
+    def add(self, start_val, end_val, duration, step_size=2, clear=False):
         self.eye_left.add(start_val, end_val, duration, step_size, clear)
         self.eye_right.add(start_val, end_val, duration, step_size, clear)
 
     def run(self):
         doubleBlink = False
         while not self.__signalExit__:
-            closeT = 0.1
-            openT = 0.15
-            self.add(self.open, self.closed, closeT)
-            self.add(self.closed, self.open, openT)
+            closeT = 0.18
+            openT = 0.32
+            self.add(float('nan'), self.closed, closeT)
+            self.add(float('nan'), self.open, openT)
             self.wait(closeT + openT)
             if doubleBlink:
                 doubleBlink = False
@@ -104,6 +116,24 @@ class HeadTurnAngularVelocity(StrategyThread):
         self.servo = self.main_thread.get_servo('head')
         self.velocity = 80  # turn speed in degrees per second
 
+    def turnNow(self):
+        # turn
+        currentAngle = self.servo.pwm_to_angle(self.servo.current_val)
+        newAngle = random.uniform(120.0, 180.0)
+        if currentAngle > 120:
+            newAngle = random.uniform(0.0, 60.0)
+        elif currentAngle > 60:
+            if random.uniform(0.0, 1.0) > 0.5:
+                newAngle = 0
+            else:
+                newAngle = 180
+
+        deltaAngle = newAngle - currentAngle
+        if deltaAngle < 0:
+            deltaAngle *= -1
+        turnTime = deltaAngle / self.velocity
+        self.servo.add(float('nan'), newAngle, turnTime, 1.0, True)
+
     def run(self):
         while not self.__signalExit__:
             # turn
@@ -113,42 +143,11 @@ class HeadTurnAngularVelocity(StrategyThread):
             if deltaAngle < 0:
                 deltaAngle *= -1
             turnTime = deltaAngle / self.velocity
-            self.servo.add(currentAngle, newAngle, turnTime, 1.0, True)
+            self.servo.add(float('nan'), newAngle, turnTime, 1.0, True)
             self.wait(turnTime)
             # wait
-            waitTime = random.uniform(3.0, 30.0) * (1.5-self.agitation)
+            waitTime = random.uniform(2.0, 10.0) * (1.5-self.agitation) * (1.5-self.agitation)
             self.wait(waitTime)
-
-
-class HeadTurnAgitation(StrategyThread):
-    """Looks to different spots faster and more frequently if more agitated.
-    agitation: float between 0 and 1
-    """
-    def __init__(self, main_thread, agitation=0.5):
-        StrategyThread.__init__(self, main_thread, 'HeadTurnAgitation')
-        self.agitation = agitation
-        self.servo = self.main_thread.get_servo('head')
-        self.maxAngleVelocity = 30  # degrees per second
-
-    def run(self):
-        last_angle = 90.0  # assuming a default
-        while not self.__signalExit__:
-            waitTime = random.uniform(20.0, 30.0) * (1.0-self.agitation)
-            logging.debug('wait %f' % waitTime)
-            self.wait(waitTime)
-            head_angle = random.uniform(0.0, 180.0)
-            turnTime = random.uniform(4.0, 20.0) * (1.0-self.agitation)
-            deltaAngle = head_angle - last_angle
-            if deltaAngle < 0:
-                deltaAngle *= -1
-            angleVelocity = deltaAngle / turnTime
-            if angleVelocity > self.maxAngleVelocity:
-                turnTime = deltaAngle / self.maxAngleVelocity
-            # logging.debug('%f %f %f' % (last_angle, head_angle, turnTime))
-            self.servo.add(float('nan'), head_angle, turnTime, 1.0, True)
-            self.wait(turnTime)
-            last_angle = head_angle
-        logging.debug('looking finished')
 
 class AutoStrategy(StrategyThread):
     """
@@ -159,35 +158,38 @@ class AutoStrategy(StrategyThread):
 
     def run(self):
         logging.debug('- using AutoStrategy.run')
-
-        # background breathing
-        breathing = BreathingAgitation(self.main_thread)
-        # breathing = HeartbeatAgitation(self.main_thread)
-        breathing.start()
-
-        blinking = BlinkingAgitation(self.main_thread)
-        blinking.start()
-
-        # looking = HeadTurnAgitation(self.main_thread)
         looking = HeadTurnAngularVelocity(self.main_thread)
         looking.start()
 
         while not self.__signalExit__:
-            self.wait(15)
-
-            agitation = random.uniform(0.0, 1.0)
-            agitation = 0.5
-            logging.debug("setting agitation to %f" % agitation)
-
-            breathing.agitation = agitation
-            blinking.agitation = agitation
-            looking.agitation = agitation
+            breathing = BreathingAgitation(self.main_thread)
+            breathing.start()
+            blinking = BlinkingAgitation(self.main_thread)
+            blinking.start()
+            looking.velocity = 60
+            looking.agitation = 0.0
+            self.wait(30)
+            breathing.signal_exit()
+            blinking.signal_exit()
+            looking.velocity = 120
+            looking.agitation = 0.9
+            looking.turnNow()
+            self.wait(2)
+            heartbeat = HeartbeatAgitation(self.main_thread, 0.75)
+            heartbeat.eye_delta = -0.1
+            heartbeat.start()
+            while not self.__signalExit__ and heartbeat.agitation > 0.2:
+                self.wait(1)
+                heartbeat.eye_delta -= 0.00005
+                heartbeat.agitation -= 0.05
+            heartbeat.signal_exit()
 
         # wait for subthread to finish
         breathing.signal_exit()
         blinking.signal_exit()
         looking.signal_exit()
-        while breathing.is_alive() or blinking.is_alive() or looking.is_alive():
+        heartbeat.signal_exit()
+        while breathing.is_alive() or blinking.is_alive() or looking.is_alive() or heartbeat.is_alive():
             logging.debug('...waiting for breathing, blinking and looking to finish gracefully...')
             sleep(0.01)
 
