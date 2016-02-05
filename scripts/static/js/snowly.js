91bd536fa2a05b634714782f8f3df02e57e92e23
Kai Jauslin <kai.jauslin@cando-image.com>
2016-02-05 14:12:42 +0100

    Added periodica and manual refresh update

--- scripts/static/js/snowly.js
+++ scripts/static/js/snowly.js
@@ -19,17 +19,25 @@
 
           $('.master-strategies').children().remove();
           $.each(data.playlist, function(i, val) {
-              button = $('<button type="button"/>').text(val).addClass(val + ' btn btn-default col-xs-12 col-md-3 col-sm-4');
+              button = $('<button type="button"/>').text(val).addClass(val + ' btn btn-default col-xs-12');
               button.click(function() {
                  setStrategy(val);
               });
               if (data['active_strategy'] == val) {
                   button.addClass('active');
               }
-              $('.master-strategies').append($('<div/>').append(button).append($('<p/>').text(data['playlist_desc'][i])));
+              $('.master-strategies').append($('<div/>').addClass('col-xs-12 col-md-3 col-sm-4').append(button).append($('<p/>').text(data['playlist_desc'][i])));
           });
         });
     }
 
     update();
+
+    // periodic refresh
+    setInterval(function() {
+        update();
+    }, 30000);
+
+    // manual refresh
+    $('.refresh').click(update);
 })(jQuery);
