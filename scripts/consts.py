d36b3c853ad13ef669648de03adfa2447939f0d9
Kai Jauslin <kai.jauslin@cando-image.com>
2016-09-01 18:06:17 +0200

    Lower manual commands timeout on master to 30s (fallback after no commands)

--- scripts/consts.py
+++ scripts/consts.py
@@ -1,8 +1,8 @@
 STATE_DISCONNECTED = 0
 STATE_CONNECTED = 1
 
-# 60s timeout receiving commands from master
-SLAVE_TIMEOUT = 240
+# timeout receiving commands from master (default 60s)
+SLAVE_TIMEOUT = 120
 
-# 30s timeout for manual commands
-IDLE_TIMEOUT = 240
+# 30s timeout for manual commands (default 240s)
+IDLE_TIMEOUT = 30
