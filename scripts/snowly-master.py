23d1a32753b4507051949e2cc0608f781438bbb7
Kai Jauslin <kj@nextension.com>
2018-11-28 17:09:26 +0000

    Fix UI websocket sync

--- scripts/snowly-master.py
+++ scripts/snowly-master.py
@@ -130,7 +130,7 @@ class SnowlyServer(Server):
 
         if websocket_support:
             params['clients'] = clients
-            cherrypy.engine.publish('websocket-broadcast', TextMessage(json.dumps(list(params))))
+            cherrypy.engine.publish('websocket-broadcast', TextMessage(json.dumps(params)))
             params.pop('clients', None)
 
         for id in clients:
